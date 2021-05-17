b3b8303b2b97b4192429abdbf3c5149dcfb35518
Roberto antoniello <roberto.antoniello@studenti.unimi.it>
2021-05-17 18:08:54 +0200

    Update FinestraAvvio.java
    A few changes and added comments into the code.

--- FinestraAvvio.java
+++ FinestraAvvio.java
@@ -14,28 +14,32 @@ public class FinestraAvvio extends JFrame{
 
 	private JButton accessoRastrelliera = new JButton("Accesso rastrelliera");
 
-	private JButton restituzione = new JButton("Restituzion");
+	private JButton restituzione = new JButton("Restituzione");
 	
 	//Costruttore
 	public FinestraAvvio() {
 		JPanel pannello = new JPanel();
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(600,200);
-		//aggiungo gli attributi sulla finestra
+		//aggiungo gli attributi componenti sul pannello
 		pannello.add(titolo);
 		pannello.add(registrazione);
 		pannello.add(accessoRastrelliera);
 		pannello.add(restituzione);
+		//aggiungo il pannello con i suoi componenti alla finestra
 		this.add(pannello);
 	}
-	public void ascoltoRegistrazione(ActionListener AscoltaBottoneRegistrazione){
-		registrazione.addActionListener(AscoltaBottoneRegistrazione);
+	//metodo usato dal controller per intercettare il click sul bottone registrazione
+	public void ascoltoRegistrazione(ActionListener bottoneRegistrazione){
+		registrazione.addActionListener(bottoneRegistrazione);
 	}
-	public void ascoltoRastrelliera(ActionListener AscoltaBottoneRastrelliera){
-		accessoRastrelliera.addActionListener(AscoltaBottoneRastrelliera);
+	//metodo usato dal controller per intercettare il click sul bottone accesso rastrelliera.
+	public void ascoltoRastrelliera(ActionListener bottoneRastrelliera){
+		accessoRastrelliera.addActionListener(bottoneRastrelliera);
 	}
-	public void ascoltoRestituzione(ActionListener AscoltaBottoneRestituzione){
-		restituzione.addActionListener(AscoltaBottoneRestituzione);
+	//metodo usato dal controller per intercettare il click sul bottone restituzione.
+	public void ascoltoRestituzione(ActionListener bottoneRestituzione){
+		restituzione.addActionListener(bottoneRestituzione);
 	}
 
 }
