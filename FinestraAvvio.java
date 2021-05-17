62a7e0457f72f67936f5d4ac8ad7eeb3ce77d6a1
Roberto antoniello <roberto.antoniello@studenti.unimi.it>
2021-05-17 16:59:12 +0200

    Update FinestraAvvio.java
    Setting up attributes and methods

--- FinestraAvvio.java
+++ FinestraAvvio.java
@@ -1,41 +1,41 @@
-
 import java.util.*;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
-/**
- * 
- */
-public class FinestraAvvio {
 
-	/**
-	 * Default constructor
-	 */
-	public FinestraAvvio() {
-	}
+public class FinestraAvvio extends JFrame{
 
-	/**
-	 * 
-	 */
+	//attributi componenti della finestra
 	private JFrame finestra;
 
-	/**
-	 * 
-	 */
-	private JLabel titolo;
+	private JLabel titolo = new JLabel("Sistema bike sharing");
 
-	/**
-	 * 
-	 */
-	private JButton registrazione;
+	private JButton registrazione = new JButton("Registrazione");
 
-	/**
-	 * 
-	 */
-	private JButton accessoRastrelliera;
+	private JButton accessoRastrelliera = new JButton("Accesso rastrelliera");
 
-	/**
-	 * 
-	 */
-	private JButton restituzione;
-
+	private JButton restituzione = new JButton("Restituzion");
+	
+	//Costruttore
+	public FinestraAvvio() {
+		JPanel pannello = new JPanel();
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(600,200);
+		//aggiungo gli attributi sulla finestra
+		pannello.add(titolo);
+		pannello.add(registrazione);
+		pannello.add(accessoRastrelliera);
+		pannello.add(restituzione);
+		this.add(pannello);
+	}
+	public void ascoltoRegistrazione(ActionListener AscoltaBottoneRegistrazione){
+		registrazione.addActionListener(AscoltaBottoneRegistrazione);
+	}
+	public void ascoltoRastrelliera(ActionListener AscoltaBottoneRastrelliera){
+		accessoRastrelliera.addActionListener(AscoltaBottoneRastrelliera);
+	}
+	public void ascoltoRestituzione(ActionListener AscoltaBottoneRestituzione){
+		restituzione.addActionListener(AscoltaBottoneRestituzione);
+	}
 
 }
