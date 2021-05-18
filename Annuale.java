8c4ea06a5b5bd4ca406a42c5a19f9ba4ea9cf531
Roberto antoniello <roberto.antoniello@studenti.unimi.it>
2021-05-18 18:21:30 +0200

    Update Annuale.java
    Basic attributes and methods written

--- Annuale.java
+++ Annuale.java
@@ -1,27 +1,22 @@
-
 import java.util.*;
 
 /**
- * 
+ * sottoclasse che estende la classe abbonamento e rappresenta in particolare l'abbonamento di tipo annuale.
+ * Eredita tutti i metodi della classe Abbonamento e implementa inizioValidita() impostando la scadenza a un anno solare dalla data odierna.
  */
 public class Annuale extends Abbonamento {
-
-	/**
-	 * Default constructor
-	 */
-	public Annuale() {
+	
+	private int prezzo = 36;
+	
+	private String scadenza;
+	
+	//costruttore
+	public Annuale(int prezzo, String scadenza) {
+		super(prezzo, scadenza);
 	}
-
-	/**
-	 * 
-	 */
-	public void inizioValidità() {
-		// TODO implement here
+	
+	public void inizioValidita() {
+		GregorianCalendar oggi = new GregorianCalendar(GregorianCalendar.YEAR,GregorianCalendar.MONTH+1,GregorianCalendar.DAY_OF_MONTH);
+		this.scadenza = String.valueOf(oggi.get(GregorianCalendar.DAY_OF_MONTH)) + "/" + String.valueOf(oggi.get(GregorianCalendar.MONTH)) + "/" + String.valueOf(oggi.get(GregorianCalendar.YEAR+1));
 	}
-
-	/**
-	 * 
-	 */
-	public abstract void inizioValidità();
-
 }
