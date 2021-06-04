b7aae648655cd47c6e8fff5911cdf70595918b5f
Roberto antoniello <roberto.antoniello@studenti.unimi.it>
2021-06-04 19:15:37 +0200

    Create singleton class
    Created a singleton class to manage database connection

---
+++ ConnessioneDB.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+/**
+ * classe singleton che gestisce la connessione con il database.
+ */
+public class ConnessioneDB{
+		
+	private  static Connection connessione = null;
+	
+	static{
+		String url = "jdbc:mysql://localhost:3306/bikesharing?user=user&password=password";
+		try{
+			connessione = DriverManager.getConnection(url);
+			System.out.println("Connessione con DB avvenuta!");
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * @return connessione
+	 */
+	public static Connection getConnesione() throws SQLException{
+		return connessione;
+	}
+}
