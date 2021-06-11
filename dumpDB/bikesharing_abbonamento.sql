41e412d18904c4f3ba2c3e658f1b18c50dcc09bd
Roberto antoniello <roberto.antoniello@studenti.unimi.it>
2021-06-11 20:32:50 +0200

    Update dump database

--- dumpDB/bikesharing_abbonamento.sql
+++ dumpDB/bikesharing_abbonamento.sql
@@ -23,12 +23,12 @@ DROP TABLE IF EXISTS `abbonamento`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `abbonamento` (
-  `utente` int NOT NULL,
-  `prezzo` int DEFAULT NULL,
-  `scadenza` varchar(10) DEFAULT NULL,
+  `codiceutente` int NOT NULL,
+  `prezzo` double DEFAULT NULL,
+  `scadenza_abbo` varchar(10) DEFAULT NULL,
   `tipologia` varchar(11) DEFAULT NULL,
-  PRIMARY KEY (`utente`),
-  CONSTRAINT `utente` FOREIGN KEY (`utente`) REFERENCES `utenti` (`codiceutente`)
+  PRIMARY KEY (`codiceutente`),
+  CONSTRAINT `utente` FOREIGN KEY (`codiceutente`) REFERENCES `utenti` (`codiceutente`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Tabella che rappresenta ogni abbonamento legato ad ogni utente.';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -50,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-04 18:30:53
+-- Dump completed on 2021-06-11 20:30:59
--- dumpDB/bikesharing_cartadicredito.sql
+++ dumpDB/bikesharing_cartadicredito.sql
@@ -23,12 +23,12 @@ DROP TABLE IF EXISTS `cartadicredito`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cartadicredito` (
-  `utente` int NOT NULL,
+  `codiceutente` int NOT NULL,
   `numerocarta` int DEFAULT NULL,
-  `scadenza` varchar(7) DEFAULT NULL,
+  `scadenza_carta` varchar(7) DEFAULT NULL,
   `residuo` double DEFAULT NULL,
-  PRIMARY KEY (`utente`),
-  CONSTRAINT `cartautente` FOREIGN KEY (`utente`) REFERENCES `utenti` (`codiceutente`)
+  PRIMARY KEY (`codiceutente`),
+  CONSTRAINT `cartautente` FOREIGN KEY (`codiceutente`) REFERENCES `utenti` (`codiceutente`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='tabella che rappresenta ogni carta di credito legata a ogni utente';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -50,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-04 18:30:53
+-- Dump completed on 2021-06-11 20:31:00
