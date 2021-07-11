c6a09785809e3033c0fcbe89f2b427e58c6e7a76
Roberto antoniello <roberto.antoniello@studenti.unimi.it>
2021-07-11 15:38:52 +0200

    :construction: Created view class for restituzione

--- FinestraRestituzione.java
+++ FinestraRestituzione.java
@@ -1,41 +1,67 @@
-
 import java.util.*;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
 /**
- * 
+ * classe view che rappresenta l'interfaccia grafica relativa alla fase di restituzione di una bici.
  */
-public class FinestraRestituzione {
+public class FinestraRestituzione extends JFrame{
+	
+	//attributi componenti della finestra 
+	private JLabel lbrastrelliera = new JLabel("numero rastrelliera");
 
-	/**
-	 * Default constructor
-	 */
+	private JLabel lbcodice = new JLabel("codice utente");
+
+	private JTextField txtrastrelliera = new JTextField(1);
+
+	private JTextField txtcodice = new JTextField(6);
+
+	private JButton btconferma = new JButton("Conferma restituzione");
+
+	//costruttore
 	public FinestraRestituzione() {
+		JPanel pannello = new JPanel();
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(600,200);
+		this.setLocationRelativeTo(null);
+		//aggiungo gli attributi al JPanel
+		pannello.add(lbrastrelliera);
+		pannello.add(txtrastrelliera);
+		pannello.add(lbcodice);
+		pannello.add(txtcodice);
+		pannello.add(btconferma);
+		//aggiungo il pannello alla finestra
+		this.add(pannello);
 	}
-
+	
 	/**
-	 * 
-	 */
-	private JLabel lbrastrelliera;
-
+	* @return txtcodice
+	*/
+	public String getTxtCodice(){
+		return this.txtcodice.getText();
+	}
 	/**
-	 * 
-	 */
-	private JLabel lbcodice;
-
+	* @param String codice
+	*/
+	public void setTxtCodice(String codice){
+		this.txtcodice.setText(codice);
+	}
+	
 	/**
-	 * 
-	 */
-	private JTextField txtrastrelliera;
-
+	* @return txtrastrelliera
+	*/
+	public String getTxtRastrelliera(){
+		return this.txtrastrelliera.getText();
+	}
 	/**
-	 * 
-	 */
-	private JTextField txtcodice;
-
-	/**
-	 * 
-	 */
-	private JButton conferma;
-
+	* @param String rastrelliera
+	*/
+	public void setTxtRastrelliera(String rastrelliera){
+		this.txtrastrelliera.setText(rastrelliera);
+	}
+	//metodo usato dal controller per intercettare il click sul bottone conferma.
+	public void ascoltoConfermaRestituzione(ActionListener bottoneConferma){
+		btconferma.addActionListener(bottoneConferma);
+	}
 
 }
